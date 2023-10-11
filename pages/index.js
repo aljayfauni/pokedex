593c3218df3dba4aefd1d0f8d7563e928005f515
DESKTOP-H5540Q0\aljay <aljayfauni@gmail.com>
2023-10-11 13:40:31 +0800

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,131 +1,90 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+// pages/somepage.js
+import axios from 'axios';
+export async function getStaticProps() {
+  try {
+    const response = await axios.get('https://pokeapi.co/api/v2/pokemon');
+    const data = response.data;
 
-export default function Home() {
+    return {
+      props: {
+        data,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      props: {
+        data: null,
+      },
+    };
+  }
+}
+
+function indexPage({ data }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div>
+      {data ? (
+        <div>
+          
+		  <div className="container mx-auto px-4">
+		  <h1 className="text-3xl font-bold text-amber-500 mt-10">Pokédex</h1>
+		  <br/>
+          <div className="pokemon-list flex flex-wrap space-y-10 ">
+            {data.results.map((pokemon, index) => (
+              <div key={index} className="pokemon-item   shadow-md bg-gray-100">
+                <a href={`#`} >
+                  <img
+                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`}
+                    alt={pokemon.name}
+                  />
+                  
+                </a>
+				<div className="btn-name bg-gray-50">
+					<button class="bg-amber-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full text-stone-900 font-medium">
+						{pokemon.name}
+					</button>
+				</div>
+              </div>
+            ))}
+          </div>
         </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
+		</div>
+      ) : (
+        <p>Data not available</p>
+      )}
 
       <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+        .pokemon-list {
+        
+          justify-content: space-between;
+		  align-items:space-between;
+		  margin-bottom:10px;
         }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
+		.btn-name{
+			height:100px;
+			width:100%;
+			padding:20px;
+		}
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
+        .pokemon-item {
+     
+          padding: px;
+          margin-bottom: 0px;
+          text-align: center;
         }
-        * {
-          box-sizing: border-box;
+
+        .pokemon-item img {
+         	width: 100%;
+			height: 200px !important;
+			object-fit: contain;
+			object-position: 50% 50%;
         }
+
+        /* You can adjust the styles as needed */
       `}</style>
     </div>
   );
 }
+
+export default indexPage;
+ 
